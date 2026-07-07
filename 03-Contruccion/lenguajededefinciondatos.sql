2cf1dc527ef9dac038f51dfb281b6a9d57eca97b
Cruz Meneses <alfonsocruzmeneses12@gmail.com>
2026-07-07 15:50:23 -0600

    Se comenzo con la documentacion de los tipos de lenguaje SQL y se trabajo utilizando SQL-LDD, la creacion de base de datos tablas y restriccio- nes de primary key, identity y not null ocupando el comando CREATE y se finalizo con el inicio de la restriccion UNIQUE

---
+++ 03-Contruccion/lenguajededefinciondatos.sql
@@ -0,0 +1,140 @@
+-- Construccion de base de datos con SQL-LDD (Create,Alter,Drop)
+
+-- Crear una base de datos
+
+CREATE DATABASE universidad;
+GO
+
+-- Usar la base de datos
+Use universidad;
+GO
+
+-- Crear una tabla
+
+CREATE TABLE alumno (
+	alumno_id INT, 
+	nombre VARCHAR(50),
+	apellido_paterno VARCHAR(25),
+	apellido_materno VARCHAR(20),
+	fecha_nacimiento DATE,
+	correo VARCHAR(50)
+);
+GO
+
+INSERT INTO alumno
+VALUES(1,'Arcadia','Vaca','Del Corral','1986-04-07','correo@correo.com');
+
+
+INSERT INTO alumno
+VALUES(1,'Kevin','Cabeza','De Borrego','1942-05-10','correo@correo.com');
+
+SELECT*
+FROM alumno;
+
+-- Restricciones 
+/*	
+	Las restricciones son reglas que garantizan la integridad de los datos
+
+	Las mas utilizadas son:
+
+	- PRIMARY KEY
+	- FOREIGN KEY
+	- NOT NULL
+	- UNIQUE
+	- CHECK
+	- DEFAULT
+*/
+
+DROP TABLE alumno;
+GO
+
+-- PRIMARY KEY
+
+CREATE TABLE alumno(
+	alumno_id INT PRIMARY KEY,
+	nombre VARCHAR(100),
+	correo VARCHAR(100),
+);
+GO
+
+INSERT INTO alumno
+VALUES(1,'Luis','correo@luis.com');
+
+INSERT INTO alumno
+VALUES(2,'Roberta','correo@luis.com');
+
+DROP TABLE alumno;
+GO
+
+CREATE TABLE alumno(
+	alumno_id INT NOT NULL,
+	nombre VARCHAR(100),
+	correo VARCHAR(100),
+	CONSTRAINT pk_alumno
+	PRIMARY KEY (alumno_id)
+);
+GO
+
+
+INSERT INTO alumno
+VALUES(1,'Luis','correo@luis.com');
+
+INSERT INTO alumno
+VALUES(1,'Roberta','correo@luis.com');
+
+DROP TABLE alumno;
+GO
+
+-- PRIMARY KEY CON IDENTITY
+
+CREATE TABLE alumno(
+	alumno_id INT IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR(100),
+	correo VARCHAR(100),
+);
+GO
+
+INSERT INTO alumno
+VALUES('Luis','correo@luis.com');
+
+INSERT INTO alumno
+VALUES('Roberta','correo@luis.com');
+
+SELECT*
+FROM alumno;
+
+DROP TABLE alumno;
+
+CREATE TABLE alumno(
+	alumno_id INT NOT NULL IDENTITY(1,1),
+	nombre VARCHAR(100),
+	correo VARCHAR(50),
+	CONSTRAINT pk_alumno
+	PRIMARY KEY (alumno_id)
+);
+GO
+
+-- NOT NULL
+
+CREATE TABLE profesor(
+	profesor_id INT NOT NULL IDENTITY(1,1),
+	numero_nomina VARCHAR(20) NOT NULL,
+	nombre VARCHAR(15) NOT NULL,
+	apellido_materno VARCHAR(20) NOT NULL,
+	apellido_paterno VARCHAR(20) NULL,
+	fecha_ingreso DATE, 
+	CONSTRAINT pk_profesor
+	PRIMARY KEY (profesor_id)
+);
+GO
+
+INSERT INTO profesor
+VALUES('56565626','Luis','Hernandez',NULL,'2027-05-07');
+
+SELECT *
+FROM profesor;
+
+INSERT INTO profesor (numero_nomina, nombre, apellido_materno)
+VALUES ('4544655','Ricarda','Sonrics')
+
+-- RESTRICCION UNIQUE
